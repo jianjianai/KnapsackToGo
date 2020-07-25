593c5ba666a503d86c5bec91c51c98e88c1bb274
冰子 <54745251+BingZi-233@users.noreply.github.com>
2020-07-25 13:33:25 +0800

    Bingzi (#6)
    * 冰子
    + 新建分支
    + 创建了锁类，方便后续进行操作。
    
    * 冰子
    + 将一些冗余声明改成了匿名函数

---
+++ src/cn/jji8/kuafubeibaotongbu/bblock.java
@@ -0,0 +1,69 @@
+package cn.jji8.kuafubeibaotongbu;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class bblock implements Lock {
+    // 锁的状态
+    private boolean isLocked = false;
+    // 正在运行的进程，开始为null
+    private Thread runningThread = null;
+    // 计数器
+    private int count = 0;
+
+    // 进程的执行体
+    @Override
+    public synchronized void lock() {
+        while (isLocked) {
+            try {
+                // 通知线程，等！
+                wait();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // 一旦发现线程空闲，立刻进行占用。
+        isLocked = true;
+        // 正在运行的进程进行更换。
+        runningThread = Thread.currentThread();
+        // 计数自增。
+        count++;
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public synchronized void unlock() {
+        if (runningThread == Thread.currentThread()) {
+            count--;
+            if (count == 0) {
+                // 修改锁的状态
+                isLocked = false;
+                // 全部释放后线程恢复null
+                runningThread = null;
+                // 通知等待线程，具体是那个抢到就不知道了
+                notifyAll();
+            }
+        }
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
